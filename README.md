9a21da8b9ab8f6b887d11c2015db3a8c973ba317
Dipankar Mitra <dipankar9612@gmail.com>
2024-06-23 00:33:44 +0530

    set up guide added in readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # CelebSync
+The CelebSync is a learning project on langChain.
+You can clone the project and work on by changing the prompt and prompt related code.
+To Set it up in local, follow the below steps-
+## Set up Guide
+1. Install conda env or any other env with python 3.12
+
+```commandline
+git clone https://github.com/TMGA-WAY/CelebSync.git
+```
+```commandline
+pip install -r requirements.txt
+```
+Create an .env file and place your OPEN_AI_API_KEY
+```commandline
+streamit run main.py
+```
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from langchain.llms import OpenAI
 from langchain import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
 
 import streamlit as st
 
@@ -18,28 +19,39 @@ input_prompt = PromptTemplate(
     template='tell me about {name}'
 )
 
+##memory
+person_memory = ConversationBufferMemory(input_key="name", memory_key="chat_history")
+dob_memory = ConversationBufferMemory(input_key="person", memory_key="chat_history")
+descr_memory = ConversationBufferMemory(input_key="dob", memory_key="chat_history")
+
 # OPENAI LLM
 llm = OpenAI(temperature=0.8, openai_api_key=open_ai_api_key)
-chain = LLMChain(llm=llm, prompt=input_prompt, verbose=True, output_key='person')
+chain = LLMChain(llm=llm, prompt=input_prompt, verbose=True, output_key='person', memory=person_memory)
 
 ## second chain
 second_prompt = PromptTemplate(
     input_variables=['person'],
     template="when was {person} born?"
 )
-chain2 = LLMChain(llm=llm, prompt=second_prompt, verbose=True, output_key='dob')
+chain2 = LLMChain(llm=llm, prompt=second_prompt, verbose=True, output_key='dob', memory=dob_memory)
 
 ## third chain
 third_prompt = PromptTemplate(
     input_variables=['dob'],
     template="Mention 5 major events happened around {dob} in the world."
 )
-chain3 = LLMChain(llm=llm, prompt=third_prompt, verbose=True, output_key='description')
-
-
+chain3 = LLMChain(llm=llm, prompt=third_prompt, verbose=True, output_key='description', memory=descr_memory)
 
 parent_chain = SequentialChain(
-    chains=[chain, chain2, chain3], input_variables=['name'], output_variables=['person', 'dob', 'description'], verbose=True)
+    chains=[chain, chain2, chain3], input_variables=['name'], output_variables=['person', 'dob', 'description'],
+    verbose=True)
 
 if input_text:
-    st.write(parent_chain({'name': input_text}))
+    parent_chain({'name': input_text})
+    # st.write(parent_chain({'name': input_text}))
+    with st.expander('Person Name'):
+        st.info(person_memory.buffer)
+    with st.expander('Born'):
+        st.info(dob_memory.buffer)
+    with st.expander('Major Events'):
+        st.info(descr_memory.buffer)
